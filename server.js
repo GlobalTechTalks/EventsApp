9ca243d9774635646e9d63df52789293db51a9d6
Nupur Agrahari <nupur.agrahari93@gmail.com>
2019-11-25 22:44:37 +0530

    app.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const config = require('./config');
 const router = require('./routes');
 
 const serverPort = config.port;
+var indexRouter = require('./routes/index');
 const app = new express();
 
 app.listen(serverPort);
+
+app.use('/', indexRouter);
 
 router.initializeRoutes(app).then(() => {
     console.log(`Server started successfully on port: ${serverPort}`);
